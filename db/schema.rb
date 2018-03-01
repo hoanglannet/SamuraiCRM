72f571c3bb75b75ac5139e2b1992ae0e91d1ea0f
Aditya Raghuwanshi <adi.version1@gmail.com>
2018-03-01 12:04:17 -0800

    Add Contact Model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180301185748) do
+ActiveRecord::Schema.define(version: 20180301200050) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "samurai_contacts_contacts", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "company"
+    t.string "email"
+    t.string "phone"
+    t.bigint "samurai_user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["samurai_user_id"], name: "index_samurai_contacts_contacts_on_samurai_user_id"
+  end
 
   create_table "samurai_users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -33,4 +45,5 @@ ActiveRecord::Schema.define(version: 20180301185748) do
     t.index ["reset_password_token"], name: "index_samurai_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "samurai_contacts_contacts", "samurai_users"
 end
